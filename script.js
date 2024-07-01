295aa7f56dec394b0faa133a8ac74d19b98ee1a8
WHORYZEN <kanishkshkl@gmail.com>
2024-07-01 23:19:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+function currentTime(){
+    let date  = new Date()
+    let hours = date.getHours() 
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    let amPM
+
+    if(hours>12){ 
+         hours = hours - 12
+         amPM = "PM"
+    }
+    else if(hours==0){
+       hours = 12
+       amPM = "AM"
+    }
+    else if(hours<12){
+       amPM = "AM"
+    }
+    else if(hours==12){
+        amPM = "PM"
+    }
+   
+    minutes = addZero(minutes)
+    seconds = addZero(seconds)
+
+    let day = date.getDay()
+    let month = date.getMonth() 
+    let year = date.getFullYear()
+
+
+   
+    clockSection.innerHTML = month+"/"+day+"/"+year + " " + hours+":"+minutes+":"+seconds+" "+amPM
+
+}
+
+setInterval(currentTime, 1000) // 1sec = 1000ms
